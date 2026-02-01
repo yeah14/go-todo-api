d0eaeafe7a1e4adc8acefe83c52bf31a1194acce
yeah <1101703918@qq.com>
2026-02-01 11:20:39 +0800

    配置管理

--- config/config.go
+++ config/config.go
@@ -1,3 +1,69 @@
 package config
 
-import ()
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	Server   SeverConfig
+	Database DatabaseConfig
+	JWT      JWTConfig
+	Redis    RedisConfig
+	Log      LogConfig
+}
+
+type SeverConfig struct {
+	Port         string
+	Mode         string
+	ReadTimeout  int
+	WriteTimeout int
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBname   string
+	Charset  string
+}
+
+type JWTConfig struct {
+	Secret        string
+	AccessExpire  int
+	RefreshExpire int
+	issuer        string
+}
+
+type RedisConfig struct {
+	Addr     string
+	Password string
+	DB       int
+}
+
+type LogConfig struct {
+	Level      string
+	File       string
+	MaxSize    int
+	MaxBackups int
+	MaxAge     int
+	Compress   bool
+}
+
+var GlobalConfig config
+
+func InitCongig(configFile string) {
+	if configFile == "" {
+		configFile = "config/config.yaml"
+	}
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(configFile)
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("读取配置文件错误：%v", err)
+	}
+	if err := viper.Unmarshal(&GlobalConfig); err != nil {
+		log.Fatal("解析配置文件错误:%v", err)
+	}
+}
