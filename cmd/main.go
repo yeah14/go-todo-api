d37d9be49f92e469bda629e7743bc72ca0079c7b
yeah <1101703918@qq.com>
2026-02-22 11:13:51 +0800

    添加swagger注释和文档

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,25 @@ import (
 	"log"
 )
 
+// @title           Go-Todo-API
+// @version         1.0
+// @description     基于 Gin 的待办事项管理系统 API
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.apikey  BearerAuth
+// @in                          header
+// @name                        Authorization
+// @description                 Type "Bearer" followed by a space and JWT token.
 func main() {
 	config.InitConfig("config/dev.yaml")
 	db, err := database.ConnectMysql(config.GlobalConfig.Database)
